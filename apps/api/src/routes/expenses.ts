7ac1cd8f80ccf10a779ec7f583c2f69a84982cd5
kamil <kamilmaslanka212@gmail.com>
2026-01-08 17:21:52 +0100

    ExpensesRoute get and post methods

--- apps/api/src/routes/expenses.ts
+++ apps/api/src/routes/expenses.ts
@@ -1,47 +1,32 @@
 import { Hono } from "hono";
 import { Expense, createPostSchema } from "@repo/schemas";
 
+import { db } from "@repo/db";
+import { expenses as expensesTable } from "@repo/db";
+
 //This function runs as a middleware between each request and response
 import { zValidator } from "@hono/zod-validator";
 
-const expenses: Expense[] = [
-  { id: 1, title: "Groceries", amount: 50 },
-  { id: 2, title: "Utilities", amount: 100 },
-  { id: 3, title: "Rent", amount: 1000 },
-];
-
 export const expensesRoute = new Hono()
-  .get("/", (c) => {
+  .get("/", async (c) => {
+    const expenses = await db.select().from(expensesTable);
+
     return c.json({ expenses: expenses });
   })
-  .get("/total", (c) => {
-    const total = expenses.reduce((sum, expense) => sum + expense.amount, 0);
-    return c.json({ total: total });
-  })
+
   .post("/", zValidator("json", createPostSchema), async (c) => {
     const expense = c.req.valid("json");
-    expenses.push({
-      id: expenses.length + 1,
-      ...expense,
+
+    const response = await db.insert(expensesTable).values({
+      title: expense.title,
+      amount: expense.amount,
     });
-    return c.json({ message: "Expense created", expenses: expenses });
+
+    return c.json({ message: "Expense created", expenses: expense });
   })
   .get("/:id{[0-9]+}", (c) => {
-    const id = Number.parseInt(c.req.param("id"));
-    const expense = expenses.find((expense) => expense.id === id);
-    if (expense) {
-      return c.json({ expense });
-    } else {
-      return c.json({ message: "Expense not found", id: id }, 404);
-    }
+    return c.json({ message: "Get expense by ID" });
   })
   .delete("/:id{[0-9]+}", (c) => {
-    const id = Number.parseInt(c.req.param("id"));
-    const index = expenses.findIndex((expense) => expense.id === id);
-    if (index !== -1) {
-      expenses.splice(index, 1);
-      return c.json({ message: "Expense deleted", id: id });
-    } else {
-      return c.json({ message: "Expense not found", id: id }, 404);
-    }
+    return c.json({ message: "Delete expense by ID" });
   });
